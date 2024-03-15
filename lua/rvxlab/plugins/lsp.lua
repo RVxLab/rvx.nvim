699f37547e71a3ad85e005d76927e22b50ab256a
RVxLab <code@rvx.works>
2024-03-15 14:59:32 +0800

    Install none-ls and set up stylua

--- lua/rvxlab/plugins/lsp.lua
+++ lua/rvxlab/plugins/lsp.lua
@@ -1,51 +1,72 @@
 -- Everything related to Language Servers
 return {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "folke/neodev.nvim",
-    },
-    config = function ()
-        -- Ensure Neodev is set up first
-        require('neodev').setup({})
+    {
+        "neovim/nvim-lspconfig",
+        event = "VeryLazy",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "folke/neodev.nvim",
+        },
+        config = function()
+            -- Ensure Neodev is set up first
+            require("neodev").setup({})
 
-        -- Initialize Mason and LSP Config
-        require('mason').setup()
+            -- Initialize Mason and LSP Config
+            require("mason").setup()
 
-        local mason_lsp = require('mason-lspconfig')
-        mason_lsp.setup({
-            ensure_installed = {
-                "rust_analyzer", -- Rust
-                "lua_ls", -- Lua
-                "phpactor", -- PHP
-                "tsserver", -- JS/TS
-            },
-        })
+            local mason_lsp = require("mason-lspconfig")
+            mason_lsp.setup({
+                ensure_installed = {
+                    "rust_analyzer", -- Rust
+                    "lua_ls", -- Lua
+                    "phpactor", -- PHP
+                    "tsserver", -- JS/TS
+                },
+            })
 
-        local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local capabilities =
+                require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-        mason_lsp.setup_handlers({
-            function (server_name)
-                require('lspconfig')[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ['lua_ls'] = function ()
-                require('lspconfig').lua_ls.setup({
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = "Replace",
+            mason_lsp.setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup({
+                        settings = {
+                            Lua = {
+                                completion = {
+                                    callSnippet = "Replace",
+                                },
                             },
                         },
-                    },
-                })
-            end,
-        })
+                    })
+                end,
+            })
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = function()
+            local none_ls = require("null-ls")
 
-    end,
+            return {
+                sources = {
+                    none_ls.builtins.formatting.stylua,
+                },
+            }
+        end,
+        init = function()
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {
+                desc = "[F]ormat the current buffer",
+            })
+        end,
+    },
 }
-
