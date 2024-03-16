f35f2f85e190255da2be668e2a45ecfd71b58b5f
RVxLab <code@rvx.works>
2024-03-16 17:58:12 +1000

    Start setting up PHPActor, PHPStan and Pint

--- lua/rvxlab/plugins/lsp.lua
+++ lua/rvxlab/plugins/lsp.lua
@@ -1,4 +1,8 @@
 -- Everything related to Language Servers
+
+-- AutoCMD groups
+local formatting_group = vim.api.nvim_create_augroup("LspFormatting", {})
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -16,35 +20,62 @@ return {
             -- Initialize Mason and LSP Config
             require("mason").setup()
 
+            -- Set up Mason's LSP integration
             local mason_lsp = require("mason-lspconfig")
             mason_lsp.setup({
                 ensure_installed = {
                     "rust_analyzer", -- Rust
-                    "lua_ls", -- Lua
-                    "phpactor", -- PHP
-                    "tsserver", -- JS/TS
+                    "lua_ls",        -- Lua
+                    "phpactor",      -- PHP
+                    "tsserver",      -- JS/TS
                 },
             })
 
+            local cmp = require("cmp")
+
+            -- Load the default capabilities for CMP and merge the built-in LSP capabilities in
             local capabilities =
                 require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-            mason_lsp.setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                completion = {
-                                    callSnippet = "Replace",
-                                },
+            -- Configuration per language server
+            -- Each key is the name of the server
+            local server_configs = {
+                lua_ls = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    settings = {
+                        Lua = {
+                            completion = {
+                                callSnippet = "Replace",
                             },
                         },
+                    },
+                },
+            }
+
+            local default_config = {
+                capabilities = capabilities,
+                on_attach = function(_, buffer)
+                    vim.keymap.set({ "n", "i" }, "<C-k>", function()
+                        -- Close any nvim-cmp window that could be open
+                        cmp.abort()
+                        vim.lsp.buf.hover()
+                    end, {
+                        buffer = buffer,
+                        desc = "Show information of whatever is being hovered over",
                     })
+                end,
+            }
+
+            -- Set up the actual LSP integrations
+            -- If a config is found in the server_config object, use that
+            -- Otherwise, use the default
+            mason_lsp.setup_handlers({
+                function(server_name)
+                    local config = vim.tbl_deep_extend("force", default_config, server_configs[server_name] or {})
+
+                    require("lspconfig")[server_name].setup(config)
                 end,
             })
         end,
@@ -58,8 +89,31 @@ return {
             local none_ls = require("null-ls")
 
             return {
+                on_attach = function(client, buffer)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({
+                            group = formatting_group,
+                            buffer = buffer,
+                        })
+
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = formatting_group,
+                            buffer = buffer,
+                            callback = function()
+                                vim.lsp.buf.format({
+                                    async = false,
+                                })
+                            end,
+                        })
+                    end
+                end,
                 sources = {
+                    -- Lua
                     none_ls.builtins.formatting.stylua,
+
+                    -- PHP
+                    none_ls.builtins.diagnostics.phpstan,
+                    none_ls.builtins.formatting.pint,
                 },
             }
         end,
