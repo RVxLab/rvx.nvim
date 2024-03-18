dcc8ebd47ca24cb27dc7ec5f47c3543c5ff4c9b8
RVxLab <code@rvx.works>
2024-03-18 09:18:32 +0800

    Nvim Notify settings

--- lua/rvxlab/plugins/ui.lua
+++ lua/rvxlab/plugins/ui.lua
@@ -74,4 +74,12 @@ return {
             }
         end,
     },
+    {
+        "rcarriga/nvim-notify",
+        version = "^3.1",
+        opts = true,
+        init = function()
+            vim.notify = require("notify")
+        end,
+    },
 }
