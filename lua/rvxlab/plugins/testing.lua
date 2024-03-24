652d75cf7b9efe018a910c072c1aef5395e3a3c3
RVxLab <code@rvx.works>
2024-03-24 17:54:16 +1000

    Add a testing plugin

---
+++ lua/rvxlab/plugins/testing.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+
+            -- Adapters
+
+            "V13Axel/neotest-pest",
+        },
+        opts = function()
+            return {
+                adapters = {
+                    require("neotest-pest"),
+                },
+            }
+        end,
+        init = function()
+            local neotest = require("neotest")
+            local util = require("rvxlab.util")
+            require("which-key").register({
+                ["<leader>t"] = { name = "[T]ests", _ = "which_key_ignore" },
+            })
+
+            vim.keymap.set("n", "<leader>tn", neotest.run.run, {
+                desc = "Test [N]earest",
+            })
+
+            vim.keymap.set("n", "<leader>tf", util.bind(neotest.run.run, vim.fn.expand("%")), {
+                desc = "Test [F]ile",
+            })
+        end,
+    },
+}
