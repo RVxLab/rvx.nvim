168fab4da576664dd7c01bc8ac44ae56e2609120
RVxLab <code@rvx.works>
2024-03-24 15:53:29 +1000

    Add a bind utils function

---
+++ lua/rvxlab/util.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+function M.bind(fn, ...)
+    local args = {...}
+
+    return function()
+        local unpack = table.unpack or unpack
+
+        return fn(unpack(args))
+    end
+end
+
+return M
