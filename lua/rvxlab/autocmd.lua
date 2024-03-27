69d953bfe2d26d4fa9c43ffb38e4e6c6824d96d2
RVxLab <code@rvx.works>
2024-03-28 07:40:36 +0800

    Remove relativenumber autocmd
    It was causing more trouble than it was worth :(

--- lua/rvxlab/autocmd.lua
+++ lua/rvxlab/autocmd.lua
@@ -2,19 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Enable/disable relative line numbers when entering/leaving insert mode
-vim.api.nvim_create_autocmd("InsertEnter", {
-    callback = function()
-        vim.wo.relativenumber = false
-    end,
-})
-
-vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = function()
-        vim.wo.relativenumber = true
-    end,
-})
-
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight_yank", {}),
