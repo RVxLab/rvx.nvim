2f56548eecba21bac6d31e8b77004b26104fa36f
RVxLab <code@rvx.works>
2024-03-27 08:38:56 +0800

    Add keymap to delete the current buffer

--- lua/rvxlab/keymap.lua
+++ lua/rvxlab/keymap.lua
@@ -2,3 +2,9 @@
 vim.keymap.set("n", "<leader>LL", ":Lazy<CR>", {
     desc = "Open [L]azy.nvim",
 })
+
+-- Delete current buffer
+
+vim.keymap.set("n", "<leader>q", ":bdelete<CR>", {
+    desc = "Close current buffer",
+})
