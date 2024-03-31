b3b3ce9409b7064c1ac0678634c994911c61b5cf
RVxLab <code@rvx.works>
2024-04-01 08:52:21 +1000

    Add Trouble

--- lua/rvxlab/plugins/ui.lua
+++ lua/rvxlab/plugins/ui.lua
@@ -25,8 +25,11 @@ return {
     },
     {
         "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "folke/trouble.nvim",
+        },
+        config = require("rvxlab.util").bound_config("rvxlab.config.lualine"),
     },
     {
         "akinsho/bufferline.nvim",
@@ -110,4 +113,41 @@ return {
         main = "ibl",
         opts = {},
     },
+    {
+        "folke/trouble.nvim",
+        branch = "dev",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+    opts = {},
 }
