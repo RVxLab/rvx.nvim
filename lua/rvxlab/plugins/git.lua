d139ef3faa03c891d946329c811fa4cedb0adb34
RVxLab <code@rvx.works>
2024-04-03 08:31:18 +0800

    Add hunk navigation

--- lua/rvxlab/plugins/git.lua
+++ lua/rvxlab/plugins/git.lua
@@ -1,3 +1,7 @@
+-- Everything related to Git
+
+local bind = require('rvxlab.util').bind
+
 return {
     {
         "lewis6991/gitsigns.nvim",
@@ -7,6 +11,19 @@ return {
             yadm = {
                 enable = false, -- I don't use yadm
             },
+            on_attach = function (buffer)
+                local gs = require('gitsigns')
+
+                vim.keymap.set('n', '[h', bind(vim.schedule, gs.prev_hunk), {
+                    desc = "Previous [H]unk",
+                    buffer = buffer,
+                })
+
+                vim.keymap.set('n', ']h', bind(vim.schedule, gs.next_hunk), {
+                    desc = "Next [H]unk",
+                    buffer = buffer,
+                })
+            end,
         },
     },
     {
@@ -22,7 +39,7 @@ return {
             "nvim-lua/plenary.nvim",
         },
         init = function()
-            vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {
+            vim.keymap.set("n", "<leader>gg", require("lazygit").lazygit, {
                 desc = "Open Lazy[G]it",
             })
         end,
