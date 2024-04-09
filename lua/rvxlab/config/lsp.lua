feac4212f8f7f5735867fb73baf39b2d1fa7fd69
RVxLab <code@rvx.works>
2024-04-10 08:22:30 +1000

    Don't require twice

--- lua/rvxlab/config/lsp.lua
+++ lua/rvxlab/config/lsp.lua
@@ -182,8 +182,7 @@ require("mason").setup()
 
 local mason_lsp = require("mason-lspconfig")
 mason_lsp.setup({ ensure_installed = util.keys(servers) })
-
-require("mason-lspconfig").setup_handlers({
+mason_lsp.setup_handlers({
     function(server)
         local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
