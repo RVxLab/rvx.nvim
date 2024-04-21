72be9c935dfa95d1970b904ac4fb476828fa2724
RVxLab <code@rvx.works>
2024-04-21 17:35:32 +1000

    More keybinds to close buffers

--- lua/rvxlab/keymap.lua
+++ lua/rvxlab/keymap.lua
@@ -1,10 +1,22 @@
--- Open Lazy
-vim.keymap.set("n", "<leader>LL", ":Lazy<CR>", {
-    desc = "Open [L]azy.nvim",
-})
+local map = require("rvxlab.util").n_keymap
 
--- Delete current buffer
+-- Lazy
+map("<leader>LL", ":Lazy<CR>", "Open [L]azy.nvim")
 
-vim.keymap.set("n", "<leader>q", ":bdelete<CR>", {
-    desc = "Close current buffer",
-})
+-- Buffer related
+map("<leader>q", ":bdelete<CR>", "Close current buffer")
+map("<leader>QA", function()
+    for _, i in ipairs(vim.api.nvim_list_bufs()) do
+        vim.api.nvim_buf_delete(i, {})
+    end
+end, "Close all buffers")
+
+map("<leader>Qc", function()
+    local current_buffer = vim.api.nvim_get_current_buf()
+
+    for _, i in ipairs(vim.api.nvim_list_bufs()) do
+        if i ~= current_buffer then
+            vim.api.nvim_buf_delete(i, {})
+        end
+    end
+end, "Close all buffers except current")
--- lua/rvxlab/plugins/ui.lua
+++ lua/rvxlab/plugins/ui.lua
@@ -20,6 +20,7 @@ return {
             require("which-key").register({
                 ["<leader>L"] = { name = "[L]azy", _ = "which_key_ignore" },
                 ["<leader>g"] = { name = "[G]eneral actions", _ = "which_key_ignore" },
+                ["<leader>Q"] = { name = "[Q]uit buffers", _ = "which_key_ignore" },
             })
         end,
     },
