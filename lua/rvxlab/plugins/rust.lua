a710fd46f78b4b35d6a96458d89fd935afa1a420
RVxLab <code@rvx.works>
2024-05-13 09:05:13 +0800

    Load Rustaceanvim after LSP Config

--- lua/rvxlab/plugins/rust.lua
+++ lua/rvxlab/plugins/rust.lua
@@ -12,14 +12,15 @@ return {
     },
     {
         "mrcjkb/rustaceanvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+        },
         version = "^4",
         lazy = false, -- This plugin is already lazy
         init = function()
             vim.g.rustaceanvim = {
                 server = {
-                    on_attach = function(_client, buffer)
-                        lsp.setup_remaps(buffer)
-                    end,
+                    on_attach = lsp.on_attach,
                 },
             }
         end,
