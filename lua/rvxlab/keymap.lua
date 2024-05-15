5bc6fca3697b72850d0a35a6865448a6fb54b581
RVxLab <code@rvx.works>
2024-05-15 14:53:10 +0800

    Add a safeguard to not close buffers or type nofile
    This has caused some issues with neotest and Lazygit

--- lua/rvxlab/keymap.lua
+++ lua/rvxlab/keymap.lua
@@ -1,13 +1,29 @@
 local map = require("rvxlab.util").n_keymap
 
+local function can_delete_buffer(buffer)
+    buffer = buffer or 0
+
+    local buftype = vim.api.nvim_get_option_value("buftype", {
+        buf = buffer,
+    })
+
+    return buftype ~= "nofile"
+end
+
 -- Lazy
 map("<leader>LL", ":Lazy<CR>", "Open [L]azy.nvim")
 
 -- Buffer related
-map("<leader>q", ":bdelete<CR>", "Close current buffer")
+map("<leader>q", function()
+    if can_delete_buffer() then
+        vim.api.nvim_buf_delete(0, {})
+    end
+end, "Close current buffer")
 map("<leader>QA", function()
     for _, i in ipairs(vim.api.nvim_list_bufs()) do
-        vim.api.nvim_buf_delete(i, {})
+        if can_delete_buffer(i) then
+            vim.api.nvim_buf_delete(i, {})
+        end
     end
 end, "Close all buffers")
 
@@ -15,7 +31,7 @@ map("<leader>Qc", function()
     local current_buffer = vim.api.nvim_get_current_buf()
 
     for _, i in ipairs(vim.api.nvim_list_bufs()) do
-        if i ~= current_buffer then
+        if i ~= current_buffer and can_delete_buffer(i) then
             vim.api.nvim_buf_delete(i, {})
         end
     end
