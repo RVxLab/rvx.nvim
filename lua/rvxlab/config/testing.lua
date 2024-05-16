7431071c884b0fdf744d73ac358a1d236fa5f75a
RVxLab <code@rvx.works>
2024-05-16 12:01:17 +0800

    Rebind test UI keymaps to be easier to use
    T is a bit harder than tu

--- lua/rvxlab/config/testing.lua
+++ lua/rvxlab/config/testing.lua
@@ -1,5 +1,6 @@
 local neotest = require("neotest")
 local util = require("rvxlab.util")
+local which_key = require("which-key")
 
 neotest.setup({
     adapters = {
@@ -15,8 +16,12 @@ neotest.setup({
 })
 
 -- Binds
-require("which-key").register({
+which_key.register({
     ["<leader>t"] = { name = "[T]ests", _ = "which_key_ignore" },
+})
+
+which_key.register({
+    ["<leader>tu"] = { name = "Test [U]I", _ = "which_key_ignore" },
 })
 
 util.n_keymap("<leader>tn", neotest.run.run, "Test [N]earest")
@@ -26,5 +31,5 @@ end, "Test [F]ile")
 util.n_keymap("<leader>tl", neotest.run.run_last, "Test [L]ast Run Test")
 util.n_keymap("<leader>td", util.bind(neotest.run.run, { strategy = "dap" }), "[D]ebug nearest test")
 
-util.n_keymap("<leader>To", neotest.output_panel.toggle, "Toggle [O]utput Panel")
-util.n_keymap("<leader>Ts", neotest.summary.toggle, "Toggle [S]ummary Panel")
+util.n_keymap("<leader>tuo", neotest.output_panel.toggle, "Toggle [O]utput Panel")
+util.n_keymap("<leader>tus", neotest.summary.toggle, "Toggle [S]ummary Panel")
