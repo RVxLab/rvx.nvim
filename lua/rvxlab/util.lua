80287d921d6da6aa5189ab2ef8cb22adf4e44c46
RVxLab <code@rvx.works>
2024-05-29 09:13:40 +0800

    Add a utility to run code depending on OS

--- lua/rvxlab/util.lua
+++ lua/rvxlab/util.lua
@@ -77,4 +77,27 @@ function M.noop()
     -- 🦗
 end
 
+---Check if the current system is macOS
+function M.is_macos()
+    return vim.fn.has("macunix")
+end
+
+---Run a function when on Mac OS or on another system
+---
+---The return value of the chosen function will be returned from this function
+---
+---@param fn_on_macos function
+---@param fn_otherwise function|nil
+function M.when_macos(fn_on_macos, fn_otherwise)
+    if M.is_macos() then
+        return fn_on_macos()
+    end
+
+    if fn_otherwise ~= nil then
+        return fn_otherwise()
+    end
+
+    return nil
+end
+
 return M
