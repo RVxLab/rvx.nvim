9ce3aad237f8f6b10bafb980c6ed3057460621f0
RVxLab <code@rvx.works>
2024-05-29 08:27:45 +0800

    Redo CMP keymappings
    The supertab was getting in the way

--- lua/rvxlab/config/cmp.lua
+++ lua/rvxlab/config/cmp.lua
@@ -5,20 +5,7 @@ local lspkind = require("lspkind")
 require("luasnip.loaders.from_snipmate").lazy_load()
 luasnip.config.setup({})
 
-local has_words_before = function()
-    -- Get the line and column of where the cursor is
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-
-    -- If we're at the start of a line it means there can't be a word in front
-    if col == 0 then
-        return false
-    end
-
-    local current_line = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-    local is_non_whitespace = current_line:sub(col, col):match("%s") == nil
-
-    return is_non_whitespace
-end
+local doc_scroll_delta = 4
 
 cmp.setup({
     preselect = false,
@@ -35,28 +22,14 @@ cmp.setup({
         format = lspkind.cmp_format(),
     },
     mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-d>"] = cmp.mapping.open_docs(),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-,>"] = cmp.mapping.scroll_docs(-doc_scroll_delta),
+        ["<C-.>"] = cmp.mapping.scroll_docs(doc_scroll_delta),
     },
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
