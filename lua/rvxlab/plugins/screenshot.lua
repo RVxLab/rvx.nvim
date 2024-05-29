60b62e47dfe2d83f1a37c9f15a80fd2aeb71d8ca
RVxLab <code@rvx.works>
2024-05-29 09:13:51 +0800

    Add nvim-silicon

---
+++ lua/rvxlab/plugins/screenshot.lua
@@ -0,0 +1,12 @@
+return {
+    "michaelrommel/nvim-silicon",
+    cmd = "Silicon",
+    opts = function()
+        return {
+            font = "Hack Nerd Font Mono=34",
+            no_window_controls = true,
+            to_clipboard = true,
+            theme = "OneHalfDark",
+        }
+    end,
+}
