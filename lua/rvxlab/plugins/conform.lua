7ed4466db088b3c54587d38b87715b0c7d83fd89
RVxLab <code@rvx.works>
2024-06-11 12:17:13 +0800

    Fix lazy loading, formatter priorities and add tsx

--- lua/rvxlab/plugins/conform.lua
+++ lua/rvxlab/plugins/conform.lua
@@ -6,26 +6,33 @@ local format_on_save_options = {
 
 return {
     "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    event = { "BufWritePre" },
+    keys = {
+        utils.lazy_key("<leader>gf", function()
+            local options = vim.tbl_deep_extend("force", format_on_save_options, {
+                async = true,
+                callback = function(err)
+                    if err ~= nil then
+                        vim.notify(err, vim.log.levels.ERROR)
+                    end
+                end,
+            })
+
+            require("conform").format(options)
+        end, "[F]ormat the current buffer"),
+    },
     opts = {
         formatters_by_ft = {
             php = { "pint", "php-cs-fixer" },
-            javascript = { "prettierd", "prettier" },
-            typescript = { "prettierd", "prettier" },
-            blade = { "prettierd", "prettier" },
+            javascript = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            blade = { { "prettierd", "prettier" } },
             nix = { "alejandra" },
             lua = { "stylua" },
             sh = { "shellcheck" },
         },
         format_on_save = format_on_save_options,
-    },
-    event = { "InsertEnter" },
-    keys = {
-        utils.lazy_key("<leader>gf", function()
-            local options = vim.tbl_deep_extend("force", format_on_save_options, {
-                bufnr = 0,
-            })
-
-            require("conform").format(options)
-        end, "[F]ormat the current buffer"),
     },
 }
