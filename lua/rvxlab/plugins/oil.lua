5c8c71098e66b10453ba19a2691c48a12d5800ce
RVxLab <code@rvx.works>
2024-06-12 07:02:16 +0800

    Fix Oil

--- lua/rvxlab/plugins/oil.lua
+++ lua/rvxlab/plugins/oil.lua
@@ -2,9 +2,6 @@ local utils = require("rvxlab.utils")
 
 return {
     "stevearc/oil.nvim",
-    -- Latest commit for 0.11 broke 0.10, locking until fixed
-    -- https://github.com/stevearc/oil.nvim/pull/411
-    commit = "96368e13e9b1aaacc570e4825b8787307f0d05e1",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
         utils.lazy_key("-", function()
