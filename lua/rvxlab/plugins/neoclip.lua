a72bdb165d02f8b3788694cf9046b4d21cf185bf
RVxLab <code@rvx.works>
2024-07-01 10:47:51 +0800

    Add SQLite support for Neoclip

--- lua/rvxlab/plugins/neoclip.lua
+++ lua/rvxlab/plugins/neoclip.lua
@@ -2,14 +2,19 @@ local utils = require("rvxlab.utils")
 
 return {
     "AckslD/nvim-neoclip.lua",
-    dependencies = { "nvim-telescope/telescope.nvim" },
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        { "kkharji/sqlite.lua", module = "sqlite" },
+    },
     event = "TextYankPost",
     keys = {
         utils.lazy_key("<leader>fc", function()
             require("telescope").extensions.neoclip.default()
         end, "Find: [C]lipboard"),
     },
-    opts = {},
+    opts = {
+        enable_persistent_history = true,
+    },
     init = function()
         require("telescope").load_extension("neoclip")
     end,
