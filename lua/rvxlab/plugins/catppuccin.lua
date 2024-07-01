5c7238a14cdf2c34000fd46f3ecf46c9297ac314
RVxLab <code@rvx.works>
2024-07-01 09:12:11 +0800

    Enable colorscheme integrations

--- lua/rvxlab/plugins/catppuccin.lua
+++ lua/rvxlab/plugins/catppuccin.lua
@@ -5,6 +5,13 @@ return {
     version = "^1.6.0",
     opts = {
         flavour = "macchiato",
+        integrations = {
+            neotest = true,
+            noice = true,
+            notify = true,
+            lsp_trouble = true,
+            which_key = true,
+        },
     },
     init = function()
         vim.cmd.colorscheme("catppuccin")
