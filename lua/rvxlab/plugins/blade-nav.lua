094236aea797ca51ac8d8fcb76ec0f58a20f78bb
RVxLab <code@rvx.works>
2024-07-06 12:24:25 +1000

    Don't conditionally load blade-nav

--- lua/rvxlab/plugins/blade-nav.lua
+++ lua/rvxlab/plugins/blade-nav.lua
@@ -4,8 +4,4 @@ return {
         "hrsh7th/nvim-cmp",
     },
     ft = { "blade", "php" },
-    cond = function()
-        local artisan_path = vim.fn.findfile("artisan", ";.")
-        return artisan_path ~= "" and vim.fn.filereadable(artisan_path)
-    end,
 }
