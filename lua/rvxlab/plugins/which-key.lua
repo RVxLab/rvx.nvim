91ef2607a1777c8ca93058fd7a8d559bd0951e6c
RVxLab <code@rvx.works>
2024-07-17 10:43:27 +1000

    Update which-key entries to follow v3 spec

--- lua/rvxlab/plugins/which-key.lua
+++ lua/rvxlab/plugins/which-key.lua
@@ -1,3 +1,14 @@
+---@param mapping string
+---@param group string
+---
+---@return table
+local function make_entry(mapping, group)
+    return {
+        mapping,
+        group = group,
+    }
+end
+
 return {
     "folke/which-key.nvim",
     event = "VimEnter",
@@ -5,41 +16,35 @@ return {
     priority = 100, -- Ensure Which Key is loaded before a lot of plugins
     init = function()
         local wk = require("which-key")
-        wk.register({
-            ["<leader>g"] = { name = "[G]o to" },
-            ["<leader>Q"] = { name = "[Q]uit buffers" },
-            ["["] = { name = "Previous" },
-            ["]"] = { name = "Next" },
-            ["<leader>x"] = { name = "Trouble" },
-            ["<leader>t"] = {
-                name = "[T]ests",
-                u = {
-                    name = "Test [U]I elements",
-                },
+        wk.add({
+            {
+                make_entry("<leader>g", "[G]o to"),
+                make_entry("<leader>Q", "[Q]uit buffers"),
+                make_entry("[", "Previous"),
+                make_entry("]", "Next"),
+                make_entry("<leader>x", "Trouble"),
+                make_entry("<leader>t", "[T]ests"),
+                make_entry("<leader>tu", "Tests: [U]I elements"),
+                make_entry("<leader>p", "[P]HP"),
+                make_entry("<leader>l", "[L]aravel"),
+                make_entry("<leader>f", "[F]ind"),
+                make_entry("<leader>d", "[D]ebugging"),
+                make_entry("<leader>h", "Git: [H]unk"),
+                make_entry("<leader>i", "[I]nlay Hints"),
+                make_entry("<leader>ri", "Refactor: [I]nline"),
+                make_entry("<leader>o", "[O]verseer"),
+                make_entry("<leader>c", "[C]ode"),
             },
-            ["<leader>p"] = { name = "[P]HP" },
-            ["<leader>l"] = { name = "[L]aravel" },
-            ["<leader>f"] = { name = "[F]ind" },
-            ["<leader>d"] = { name = "[D]ebugging" },
-            ["<leader>h"] = { name = "Git: [H]unk" },
-            ["<leader>i"] = { name = "[I]nlay Hints" },
-            ["<leader>r"] = {
-                name = "[R]efactor",
-                i = { name = "Refactor: [I]nline" },
+            {
+                mode = { "v", "n" },
+                make_entry("<leader>r", "[R]efactor"),
             },
-            ["<leader>o"] = { name = "[O]verseer" },
-            ["<leader>c"] = { name = "[C]ode" },
-        })
-
-        wk.register({
-            ["<leader>s"] = { name = "Screenshot" },
-            ["<leader>r"] = {
-                name = "[R]efactor",
-                e = { name = "Refactor: [E]xtract" },
-                i = { name = "Refactor: [I]nline" },
+            {
+                mode = "v",
+                make_entry("<leader>s", "Screenshot"),
+                make_entry("<leader>e", "Refactor: [E]xtract"),
+                make_entry("<leader>i", "Refactor: [I]nline"),
             },
-        }, {
-            mode = "v",
         })
     end,
 }
