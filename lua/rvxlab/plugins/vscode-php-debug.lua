7c33622f18f238afc1abe6cad82b4ac1e13f241f
RVxLab <code@rvx.works>
2024-09-20 18:55:10 +1000

    Add PHP DAP config

--- lua/rvxlab/plugins/vscode-php-debug.lua
+++ lua/rvxlab/plugins/vscode-php-debug.lua
@@ -6,11 +6,21 @@ return {
     ft = "php",
     build = "npm ci && npm run build",
     init = function()
-        require("dap").adapters.php = {
+        local dap = require("dap")
+        dap.adapters.php = {
             type = "executable",
             command = "node",
             args = {
                 vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-php-debug/out/phpDebug.js"),
+            },
+        }
+
+        dap.configurations.php = {
+            {
+                type = "php",
+                request = "launch",
+                name = "Listen for xDebug",
+                port = 9003,
             },
         }
     end,
