560ccb0c30a2da72a87b6ef0ee94c1599be3e296
RVxLab <code@rvx.works>
2024-11-18 17:07:26 +1000

    Fix keybinds

--- lua/rvxlab/plugins/phpactor.lua
+++ lua/rvxlab/plugins/phpactor.lua
@@ -1,8 +1,8 @@
 local utils = require("rvxlab.utils")
-local lsp = require("rvxlab.lsp")
 
 return {
     "gbprod/phpactor.nvim",
+    enabled = false,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "neovim/nvim-lspconfig",
@@ -12,9 +12,9 @@ return {
         utils.lazy_key("<leader>pm", function()
             require("phpactor").rpc("context_menu", {})
         end, "Open Phpactor Context [M]enu"),
-        utils.lazy_key("<leader>pn", function()
+        utils.lazy_key("<leader>pnc", function()
             require("phpactor").rpc("new_class", {})
-        end, "Open Phpactor Context [M]enu"),
+        end, "[N]ew PHP Class"),
     },
     opts = {
         lspconfig = {
