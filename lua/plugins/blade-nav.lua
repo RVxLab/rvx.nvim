5e3e8261ca529382d918ae7bec5c96192b747225
RVxLab <code@rvx.works>
2024-12-01 23:04:50 +1000

    Prepare blade-nav support

---
+++ lua/plugins/blade-nav.lua
@@ -0,0 +1,13 @@
+local add, later = MiniDeps.add, MiniDeps.later
+
+-- Needs support from the plugin
+later(function()
+    -- add({
+    --     source = "ricardoramirezr/blade-nav.nvim",
+    --     depends = {
+    --         "saghen/blink.cmp",
+    --     },
+    -- })
+    --
+    -- require("blade-nav").setup({})
+end)
