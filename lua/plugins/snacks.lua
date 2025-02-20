c1654233d87d48c384b1862744f8d98e55935095
RVxLab <code@rvx.works>
2025-02-20 20:00:02 +1000

    Enabled Snacks.picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ now(function()
     require("snacks").setup({
         input = { enabled = true },
         notifier = { enabled = true },
+        picker = { enabled = true },
         scroll = { enabled = true },
     })
 
