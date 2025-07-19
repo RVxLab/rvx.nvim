390f570393b2f14c65ab56bc6c78c09a5bf61277
RVxLab <code@rvx.works>
2025-07-19 15:16:01 +1000

    Add blink.cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets"},
+    version = "^1.0",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = {
+            preset = "super-tab",
+        },
+    },
+}
