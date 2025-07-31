65738b98c6b3768752003b42db2ffd505e55d3c4
RVxLab <code@rvx.works>
2025-08-01 07:33:39 +1000

    Add Treesitter

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,16 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+        opts = {
+            highlight = {
+                enable = true,
+            },
+        },
+    },
+    {
         "saghen/blink.indent",
         --- @module 'blink.indent'
         --- @type blink.indent.Config
